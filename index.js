afca780e81e54e4fdc3cbf7355342c25995301bb
Farukkhan25 <programmingherofaruk@gmail.com>
2022-11-25 21:04:50 +0600

    Read all categories and products

--- index.js
+++ index.js
@@ -12,12 +12,35 @@ app.use(express.json());
 console.log(process.env.DB_USER);
 
 // Database Connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbydxwz.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
+    const cetegoriesCollection = client.db("bestBags").collection("categories");
+    const productsCollection = client.db("bestBags").collection("products");
+
+    // Get all Products
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const allProducts = await cursor.toArray();
+      res.send(allProducts);
+    });
+
+    // Get all Categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = cetegoriesCollection.find(query);
+      const allCategories = await cursor.toArray();
+      res.send(allCategories);
+    });
 
     console.log("Database Connected...");
   } finally {
